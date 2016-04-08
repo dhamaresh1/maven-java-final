d9fe7cfde6dae161380d286aa25082ef83ef34a4
dhamaresh1 <kumar2k88@yahoo.com>
2016-04-08 12:57:04 +0100

    First Maven Project Commit

---
+++ my-app/src/main/java/com/mycompany/app/SimpleDate.java
@@ -0,0 +1,13 @@
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDate {
+	 public static void main(String[] args){
+	    Date date = new Date();
+	    String strDateFormat = "HH:mm:ss a";
+	    SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
+	    System.out.println(sdf.format(date));
+	   }
+	}
+
